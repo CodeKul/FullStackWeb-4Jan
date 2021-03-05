2b438a66a417e19bae4074c72a2eac036c03ad70
chaudharisucheta <chaudharisucheta@gmail.com>
2021-03-05 15:41:04 +0530

    modules

---
+++ Javascript/main.js
@@ -0,0 +1,7 @@
+function greeting() {
+  console.log("Welcome");
+}
+function add(a, b) {
+  return a + b;
+}
+export { greeting, add };
---
+++ Javascript/moduledefault.js
@@ -0,0 +1,11 @@
+class Person{
+    constructor(city,age){
+        this.city=city
+        this.age=age
+    }
+    getPerson(){
+        console.log(this.city,this.age)
+    }
+}
+
+export default Person
